374f0fbeed58f799d147b0c475a7e3525c0b3df3
Cam Pierce <cam@campierce.dev>
2020-12-05 13:25:38 -0500

    Refactor day 1
    Discussed this with Jer last night and realized: this could be better.
    - "Function-ized" both parts; p2 now actually breaks the outer loop when
    it finds the answer
    - p1 now initializes an empty `seen` set; decided not to assume no
    duplicates, though it is true of the input
    - Inner loop in p2 uses range rather than a `nums` slice; this was lazy
    to begin with

--- 2020/day01/part1.py
+++ 2020/day01/part1.py
@@ -2,9 +2,13 @@ from aocd import get_data
 data = get_data(year=2020, day=1)
 
 nums = [int(x) for x in data.split('\n')]
-seen = set(nums)
-for n in nums:
-    x = 2020 - n
-    if x in seen:
-        print(x * n)
-        break
+
+def two_sum(target):
+    seen = set()
+    for n in nums:
+        x = target - n
+        if x in seen:
+            return x * n
+        seen.add(n)
+
+print(two_sum(2020))
--- 2020/day01/part2.py
+++ 2020/day01/part2.py
@@ -2,11 +2,14 @@ from aocd import get_data
 data = get_data(year=2020, day=1)
 
 nums = [int(x) for x in data.split('\n')]
-seen = set()
-for i, n1 in enumerate(nums):
-    for n2 in nums[i+1:]:
-        x = 2020 - n1 - n2
-        if x in seen:
-            print(x * n1 * n2)
-            break
-    seen.add(n1)
+
+def three_sum(target):
+    seen = set()
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            x = target - nums[i] - nums[j]
+            if x in seen:
+                return x * nums[i] * nums[j]
+        seen.add(nums[i])
+
+print(three_sum(2020))
