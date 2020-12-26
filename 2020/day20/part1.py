dcbbd4b6367ccc21c04adb4347bb527effff319b
Cam Pierce <cam@campierce.dev>
2020-12-26 10:55:27 -0500

    Clean up d20p1

--- 2020/day20/part1.py
+++ 2020/day20/part1.py
@@ -1,26 +1,29 @@
 import re
-from collections import Counter
+from collections import defaultdict
 from aocd import get_data
 data = get_data(year=2020, day=20)
 
 tiles = data.split('\n\n')
 
-outer, borders = {}, []
-for t in tiles:
-    head, body = t.split('\n', 1)
-    tkey = int(re.search(r'(\d+)', head)[1])
-    grid = body.split('\n')
-    bord = []
-    for i in (-1, 0):
-        bord.append(grid[i])
-        bord.append(''.join([row[i] for row in grid]))
-    norm = [sorted([b, ''.join(reversed(b))])[0] for b in bord]
-    outer[tkey] = norm
-    borders.extend(norm)
+def edges(grid):
+    grid = grid.split('\n')
+    return [''.join([row[0] for row in grid]), grid[0],
+            ''.join([row[-1] for row in grid]), grid[-1]]
 
-ctr = Counter(borders)
+def nodir(e):
+    return min(e, e[::-1])
+
+tmap = {}
+edgetotile = defaultdict(list)
+for tile in tiles:
+    head, grid = tile.split('\n', 1)
+    tk = int(re.search(r'(\d+)', head)[1])
+    tmap[tk] = grid
+    for e in [nodir(e) for e in edges(grid)]:
+        edgetotile[e].append(tk)
+
 ans = 1
-for tkey, norm in outer.items():
-    if sum([ctr[b] for b in norm]) == 6:
-        ans *= tkey
+for tk, grid in tmap.items():
+    if sum([len(edgetotile[nodir(e)]) for e in edges(grid)]) == 6:
+        ans *= tk
 print(ans)
